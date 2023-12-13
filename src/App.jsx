9b8e1d6ae8c1ea6a876d0a816213914a6cd0c9fa
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-13 17:31:16 +0600

    initialize all components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='text-5xl text-purple-500'  >Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
