15441b0a0806f2d5dcc0c09c4b6d740e3f4c6b91
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-15 22:36:59 +0600

    create event handler on the form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,11 +1,27 @@
 //import React from 'react';
 
 const AddCoffee = () => {
+    const handleAddCoffee= (event) =>{
+        event.preventDefault();
+        const name= event.target.name.value;
+        const quantity= event.target.quantity.value;
+        const supplier= event.target.supplier.value;
+        const taste= event.target.taste.value;
+        const category= event.target.category.value;
+        const details= event.target.details.value;
+        const photo= event.target.photo.value;
+
+        console.log(name,quantity,supplier,taste,category,details,photo);
+        const newCoffee= {name,quantity,supplier,taste,category,details,photo};
+        console.log(newCoffee)
+
+    }
+
     return (
         <div className="p-24 bg-[#F4F3F0]" >
             <h2 className="text-3xl font-extrabold" >Add a Coffee</h2>
 
-      <form >
+      <form onSubmit={handleAddCoffee} >
     {/* form name and quantity row */}
        <div className="md:flex">
 
@@ -40,7 +56,7 @@ const AddCoffee = () => {
 
      <div className="form-control md:w-1/2 "  >
         <label className="label">
-       <span className="label-text" > Supplier </span>
+       <span className="label-text" > Supplier Name </span>
         </label>
        
        <label className="input-group" >
@@ -109,23 +125,14 @@ const AddCoffee = () => {
 
      </div>
 
-     {/* <div className="form-control md:w-1/2 ml-4"  >
-        <label className="label">
-       <span className="label-text" > Avaliable Quantity </span>
-        </label>
-       
-       <label className="input-group" >
-         {/* <span>Name</span> */}
-         {/* <input type="text" placeholder="Quantity" name='quantity' className="input input-bordered w-full " />
-       </label>
-
-     </div>  */}
+   
 
        </div>
 
        <input type="submit" value="Add Coffee" className="btn btn-block mt-4 bg-indigo-600 " />
 
       </form>
+
 
         </div>
     );
