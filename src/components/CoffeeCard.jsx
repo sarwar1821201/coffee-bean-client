03284aa672aee8d06b6a0c72fb37a935213dfb7b
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-17 21:08:17 +0600

    delete a coffee from client site

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,45 @@
 //import React from 'react';
 
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({coffee}) => {
-    const {name,quantity,supplier,taste,category,details,photo} =coffee;
+    const {_id,name,quantity,supplier,taste,category,details,photo} =coffee;
+
+    const handleDeleteButton= (_id)=> {
+        console.log('dilam delete kore', _id )
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+          
+            console.log('delete confirm')
+           
+            fetch(`http://localhost:5000/coffee/${_id}`, {
+              method: 'DELETE'
+            }  )
+            .then(  res=> res.json() )
+            .then(data=> {
+              console.log(data)
+              if(data.deletedCount >0 ){
+                   Swal.fire({
+              title: "Deleted!",
+              text: "Your Coffee has been deleted.",
+              icon: "success"
+            });
+              }
+            })
+
+          }
+        });
+    }
+
     return (
         
       <div className="card card-side bg-base-100 shadow-xl">
@@ -18,8 +56,10 @@ const CoffeeCard = ({coffee}) => {
         <div className="card-actions justify-end">
         <div className="join join-vertical space-y-4 ">
   <button className="btn join-item">View</button>
+  <Link to={`updateCoffee/${_id}`} >
   <button className="btn join-item">Edit</button>
-  <button className="btn join-item">X</button>
+  </Link>
+  <button onClick={()=> handleDeleteButton(_id) }  className="btn bg-orange-500 join-item">X</button>
 </div>
         </div>
       </div>
--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,9 +1,15 @@
 //import React from 'react';
 
+import { useLoaderData } from "react-router-dom";
+
 const UpdateCoffee = () => {
+
+   const coffee= useLoaderData();
+   const {_id,name,quantity,supplier,taste,category,details,photo} =coffee;
+
     return (
         <div>
-            <h2>Update a Coffee</h2>
+            <h2>Update a Coffee {name} </h2>
         </div>
     );
 };
